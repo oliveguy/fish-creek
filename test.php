9949f36c011278b37ca1284e91737ac98a14cdff
Collin Park <oliveguy85@gmail.com>
2022-10-11 02:05:49 -0600

    AJAX & API run by JSON test

--- test.php
+++ test.php
@@ -8,23 +8,14 @@
     if(!$connection){
         die(mysql_connect_error());
     } 
+    $id = $_GET['id'];
 
-    $query = "SELECT * FROM monster_site";
+    $query = "SELECT * FROM monster_site WHERE id=$id";
     $sql = mysqli_query($connection,$query);
     $row = mysqli_fetch_array($sql);
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Test</h1>
-    <?php
-        echo $row[1];
+
+    echo '
+    {"monsterName":'.'"'.$row['monster_name'].'"'.','.'"monsterLv":'.'"'.$row['monster_lv'].'"
+    }
+    ';
     ?>
-</body>
-</html>
